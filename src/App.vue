e78d1e8007e45fb739380f2d0b264ddbfb89b8fb
memory-chen <chfs_002@sina.com>
2018-04-13 01:45:23 +0800

    首页更改

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,33 @@
 <template>
   <div class="layout">
     <div class="header">
-      <div class="logo">
-        Muse-UI
-      </div>
-      <div class="nav">
-        <mu-tabs :value="activeTab" @change="handleTabChange" class="tab">
-          <mu-tab value="tab1" title="Nav One" />
-          <mu-tab value="tab2" title="Nav TwO" />
-          <mu-tab value="tab3" title="Nav Three" />
-        </mu-tabs>
-      </div>
+      <mu-appbar title="Title">
+        <mu-icon-button icon="menu" slot="left" />
+        <mu-text-field icon="search" class="appbar-search-field" slot="right" hintText="请输入搜索内容" @change="submit" />
+        <mu-flat-button color="white" label="flat Button" slot="right" />
+      </mu-appbar>
     </div>
     <div class="content">
       <div class="content-left">
-        <mu-list @change="handleListChange" :value="activeList">
-          <mu-list-item title="Sent mail" value="list1">
-            <mu-icon slot="left" value="send" />
-          </mu-list-item>
-          <mu-list-item title="Drafts" value="list2">
-            <mu-icon slot="left" value="drafts" />
-          </mu-list-item>
-          <mu-list-item title="Inbox" toggleNested value="list3">
-            <mu-icon slot="left" value="inbox" />
-            <mu-list-item slot="nested" title="Starred" value="list4">
-              <mu-icon slot="left" value="grade" />
-            </mu-list-item>
-            <mu-list-item slot="nested" disabled title="Sent mail" toggleNested value="list5">
-              <mu-icon slot="left" value="send" />
-              <mu-list-item title="Drafts" slot="nested" value="list6">
-                <mu-icon slot="left" value="drafts" />
-              </mu-list-item>
-            </mu-list-item>
-            <mu-list-item slot="nested" title="Inbox" toggleNested value="list7">
-              <mu-icon slot="left" value="inbox" />
-              <mu-list-item title="Drafts" slot="nested" value="list8">
-                <mu-icon slot="left" value="drafts" />
-              </mu-list-item>
-            </mu-list-item>
-          </mu-list-item>
-        </mu-list>
+        <content-left></content-left>
+      </div>
+      <div class="content-center">
+        <content-center></content-center>
       </div>
       <div class="content-right">
-        <div class="breadcrumb">
-          <mu-breadcrumb>
-            <mu-breadcrumb-item href="/">Home</mu-breadcrumb-item>
-            <mu-breadcrumb-item href="/">VideoGame</mu-breadcrumb-item>
-            <mu-breadcrumb-item>Download</mu-breadcrumb-item>
-          </mu-breadcrumb>
-        </div>
-        <div class="body">
-          <mu-sub-header>阳光</mu-sub-header>
-          <mu-content-block>
-            散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光，找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
-          </mu-content-block>
-        </div>
+        <content-right></content-right>
       </div>
     </div>
     <div class="footer">
-      Muse-UI ©2017 Created by Muse-UI
+      martin-chen ©2018 Created by vue-dataMock
     </div>
   </div>
 </template>
 
 <script>
+  import ContentLeft from '@/containers/ContentLeft'
+  import ContentCenter from '@/containers/ContentCenter'
+  import ContentRight from '@/containers/ContentRight'
   export default {
     data() {
       return {
@@ -77,7 +41,15 @@
       },
       handleListChange(val) {
         this.activeList = val
+      },
+      submit(val) {
+        console.log(val.target.value)
       }
+    },
+    components: {
+      ContentLeft,
+      ContentRight,
+      ContentCenter
     }
   }
 </script>
@@ -85,6 +57,9 @@
 <style scoped>
   .layout {
     background-color: rgb(236, 236, 236);
+    display: flex;
+    flex-flow: column;
+    /* height: 100vh; */
   }
   .header {
     background-color: #7e57c2;
@@ -106,20 +81,29 @@
     background-color: rgba(0, 0, 0, 0);
   }
   .content {
-    overflow: hidden;
+    /* overflow: hidden; */
+    flex: 1;
+    display: flex;
   }
   .content-left {
-    width: 30%;
+    flex: 3;
     float: left;
     background-color: white;
-    margin-bottom: -4000px;
-    padding-bottom: 4000px;
+    padding: 10px 10px;
+    background-color: rgba(0, 0, 0, 0)
+    /* margin-bottom: -4000px; */
+    /* padding-bottom: 4000px; */
+  }
+  .content-center {
+    flex: 6;
+    padding: 10px 10px;
+    background-color: rgba(0, 0, 0, 0)
   }
   .content-right {
-    width: 70%;
+    flex: 3;
     display: inline-block;
     float: right;
-    padding: 10px 20px;
+    padding: 10px 10px;
     background-color: rgba(0, 0, 0, 0)
   }
   .breadcrumb {
@@ -133,5 +117,6 @@
   .footer {
     padding: 20px 0;
     text-align: center;
+    flex: 0
   }
 </style>
